a5cb79eb244d9deb315972cc4c7c281b792b6f38
Tihomir Culig <tihomirculig@gmail.com>
2024-11-12 13:31:04 +0100

    finished footer rewrite

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { Row, Col } from 'react-bootstrap';
-import "./style.css"
 import { graphql, useStaticQuery } from 'gatsby';
 import * as Styled from './styles'
+import { Button } from 'tiho-component-library';
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -84,39 +84,37 @@ const Footer = () => {
               </Styled.PadRight>
             </div>
           </Col>
-          <Col md={3} xs={12} className="t-subscribe-module">
-            <p className="t-subscribe-module-heading">Subscribe to Exodus</p>
-            <p className="t-subscribe-module-copy">
+          <Col md={3} xs={12}>
+            <Styled.Heading>Subscribe to Exodus</Styled.Heading>
+            <Styled.Subheading>
               Sign up to receive our newsletter with updates about your wallet.
-            </p>
-            <a href="/newsletter/" className="t-button t-download t-button_signup">
-              Sign me up
-            </a>
+            </Styled.Subheading>
+            <Button variant={'primary'} size={'medium'} style={{ width: "240px", height: "48px" }} onClick={() => { window.open("/newsletter/") }}>Sign me up</Button>
           </Col>
         </Row>
 
-        <Row className="t-footer-content-center">
-          <Col md={6} xs={12}>
-            <div className="t-copyright">
-              <div className="t-copyright-logo">
+        <Row>
+          <Col md={6} xs={12} style={{ padding: "60px 0" }}>
+            <div className="d-flex align-items-center">
+              <div className="pe-5">
                 <img
                   src={data?.allContentfulHeaderContent?.nodes[0]?.exodusLogo?.url}
                   alt="Exodus: Digital blockchain products"
                   loading="lazy"
                 />
               </div>
-              <div className="t-copyright-copy">
+              <Styled.CopyrightText>
                 Copyright © 2024 Exodus Movement, Inc.
                 <br />
                 Exodus was co-founded by Daniel Castagnoli and JP Richardson.
-              </div>
+              </Styled.CopyrightText>
             </div>
           </Col>
         </Row>
 
-        <Row className="t-footer-content-bottom">
+        <Row>
           <Col xs={12}>
-            <div className="t-terms">
+            <Styled.Terms>
               <span>
                 Exodus is a software platform ONLY and does not conduct any independent diligence on or substantive review of any blockchain asset, digital currency, cryptocurrency or associated funds. You are fully and solely responsible for evaluating your investments, for determining whether you will swap blockchain assets based on your own, and for all your decisions as to whether to swap blockchain assets with the Exodus in app swap feature. In many cases, blockchain assets you swap on the basis of your research may not increase in value, and may decrease in value. Similarly, blockchain assets you swap on the basis of your research may increase in value after your swap.
               </span>
@@ -131,7 +129,7 @@ const Footer = () => {
                 <a target="_blank" rel="noreferrer noopener" href="/terms/">Terms of Service</a>
                 <a target="_blank" rel="noreferrer noopener" href="/trademarks/">Trademarks</a>
               </span>
-            </div>
+            </Styled.Terms>
           </Col>
         </Row>
       </Styled.RootContainer >
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const subheadingCSS = css`
+    text-decoration: none;
+    color: #ffffff80;
+    display: block;
+    font-size: 15px;
+    font-weight: 300;
+    line-height: 1.87;
+    transition: all 0.2s linear;
+    padding: 0;
+`;
 
 export const RootContainer = styled.div`
     box-sizing: initial;
@@ -20,17 +31,46 @@ export const Heading = styled.p`
     margin: 0 0 12px;
 `;
 
+export const Subheading = styled.p`
+    ${subheadingCSS}
+`;
+
 export const Link = styled.a`
-    text-decoration: none;
-    color: #ffffff80;
-    display: block;
-    font-size: 15px;
-    font-weight: 300;
-    line-height: 1.87;
-    transition: all 0.2s linear;
-    padding: 0;
+    ${subheadingCSS}
 
     &:hover {
         color: #ffffff;
+    }
+`;
+
+export const CopyrightText = styled.div`
+    border-left: 1px solid #313243;
+    color: #ffffff4d;
+    font-size: 12px;
+    font-weight: 400;
+    margin-bottom: 5px;
+    padding-left: 40px;
+    text-align: left;
+`;
+
+export const Terms = styled.div`
+    a {
+        color: #ffffff4d;
+        display: inline-block;
+        text-decoration: underline;
+        transition: all 0.2s linear;
+    }
+
+    a + a {
+        margin-left: 10px;
+    }
+
+    span {
+        color: #ffffff4d;
+        display: block;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 1.67;
+        padding-bottom: 25px;
     }
 `;
